0492f71040314120323c7a05a01233c2ca2fe98a
tcsullivan <tullivan99@gmail.com>
2018-12-02 16:03:53 -0500

    added license

--- arm-stmos/create-toolchain.sh
+++ arm-stmos/create-toolchain.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 #
+# @file create-toolchain.sh
+# A script to build the arm-stmos toolchain
+# 
+# Copyright (C) 2018 Clyne Sullivan
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 # Based on:
 # binutils 2.31.1
 # gcc 8.2.0
 #
-# Need:
-# build-essential
-# bison
-# flex
-# libgmp3-dev
-# libmpc-dev
-# libmpfr-dev
 
 # Get sources
 if [ ! -d ./binutils-2.31.1 ]; then
